6740d1993c81c892f35f2e787f927b6ce26045cd
ShevtsovSergey <shevtsov.sergey31@gmail.com>
2016-10-06 17:23:18 +0300

    Initial

---
+++ src/main/java/ru/shevtsov/caloriesmng/web/UserServlet.java
@@ -0,0 +1,21 @@
+package ru.shevtsov.caloriesmng.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dead_rabbit
+ * 06.10.2016
+ */
+public class UserServlet extends HttpServlet {
+
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("RequestDispatcher userList");
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+    }
+}
