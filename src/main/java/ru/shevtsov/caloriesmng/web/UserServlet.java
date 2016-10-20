d9d64f1fe83ed06eab7d73c2fec942335a49fa4c
ShevtsovSergey <shevtsov.sergey31@gmail.com>
2016-10-21 00:09:35 +0300

    Add localization Add jsp fragments Out user table in jsp
    CHECK TOMCAT

--- src/main/java/ru/shevtsov/caloriesmng/web/UserServlet.java
+++ src/main/java/ru/shevtsov/caloriesmng/web/UserServlet.java
@@ -1,7 +1,11 @@
 package ru.shevtsov.caloriesmng.web;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.shevtsov.caloriesmng.LoggerWrapper;
+import ru.shevtsov.caloriesmng.service.UserService;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,9 +19,20 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet {
 
     private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+    private WebApplicationContext wac;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        wac = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.debug("RequestDispatcher userList");
+        UserService us = wac.getBean(UserService.class);
+        request.setAttribute("userList", us.getAll());
         request.getRequestDispatcher("/userList.jsp").forward(request, response);
+
+        //response.sendRedirect("userList.jsp");
     }
 }
